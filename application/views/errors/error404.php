e376d746113041ab0b103a85377a9ac0237b0fc4
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2018-04-19 15:01:26 +0700

    setting error 404 fixed

--- application/views/errors/error404.php
+++ application/views/errors/error404.php
@@ -2,9 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title><?php if($this->uri->segment(2)==""){echo "Pilih Surat - E-Surat Mercu Buana";}
-  else if($this->uri->segment(2)=="lihat"){echo "Status Surat - E-Surat Mercu Buana";}
-  else if($this->uri->segment(2)=="formkp") {echo "Surat Kerja Praktek - E-Surat Mercu Buana";} ?></title>
+  <title>Halaman Tidak ditemukan!</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('assets/favicon/apple-touch-icon.png') ?>">
   <link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url('assets/favicon/favicon-32x32.png') ?>">
@@ -33,7 +31,7 @@
             <div class="msg">X<span class="triangle"></span></div>
           </div>
           <h2 class="h1">Maaf, halaman tidak tersedia!</h2>
-          <a href="<?php echo site_url('')?>"><button class="btn btn-danger">Kembali ke Beranda</button></a>
+          <a href="<?php echo site_url('')?>"><button class="btn" style="background-color:#5DADE2; color:white;">Kembali ke Beranda</button></a>
         </div>
       </div>
     </div>
@@ -55,68 +53,7 @@
 <script src="<?php echo base_url('assets/plugins/datatables.net/js/jquery.dataTables.min.js')?>"></script>
 <script src="<?php echo base_url('assets/plugins/datatables.net-bs/js/dataTables.bootstrap.min.js')?>"></script>
 <script src="<?php echo base_url('assets/dist/js/wow.min.js') ?>"></script>
-<script>
-  function isChecked(checkbox, sub1) {
-    document.getElementById(sub1).disabled = !checkbox.checked;
-  }
-  new WOW().init();
 
-  $(function(){
-    $('#datatable').DataTable({
-      'paging'      : false,
-      'lengthChange': false,
-      'searching'   : false,
-      'ordering'    : true,
-      'info'        : true,
-      'autoWidth'   : true
-    })
-  })
-  $(function(){
-    $('#datatable2').DataTable({
-      'paging'      : false,
-      'lengthChange': false,
-      'searching'   : false,
-      'ordering'    : true,
-      'info'        : true,
-      'autoWidth'   : true
-    })
-  })
-
-  $('.btn-add').click(function(){
-
-   var x=
-   $( ".form-test" ).append( "" );
- });
-
-   // Event handler for text input
-   $('#kodenim').on('input', function() {
-    //TODO Getiing option based on input value and setting it as selected
-    $('#prodi option:contains(' + this.value + ')').eq(0).prop('selected', true);
-  });
-
-  // Event handler for select
-  $('#prodi').change(function() {
-      // Updating text input based on selected value
-      $('#kodenim').val($('option:selected', this).val());
-    });
-
-  function no(evt) {
-    var charCode = (evt.which) ? evt.which : event.keyCode
-    if (charCode > 31 && (charCode < 48 || charCode > 57))
-      return false;
-  }
-
-
-<script>
-  $(window).scroll(function(){
-    if($(document).scrollTop() > 300){
-      $('nav').addClass('shrink');
-    }
-    else{
-      $('nav').removeClass('shrink');
-    }
-  });
-</script>
 
 
 
