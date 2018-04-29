8ae95cbe5329ec8d5cc536db61a66a631e52f342
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2018-04-29 16:57:50 +0700

    fix pilihan

--- application/views/mahasiswa/pilihan.php
+++ application/views/mahasiswa/pilihan.php
@@ -43,7 +43,20 @@
 										<li>Pastikan sedang mengambil matkul KP</li>
 										<li>SKS minimal 110</li>
 									</ol>
+								</div>
+								</div>
 							</li>
+							<li class="wow bounceInRight">
+							<div class="timeline-badge danger"><i class="glyphicon glyphicon-user"></i></div>
+							<div class="timeline-panel" style="background-color: #FFFFFF;">
+								<div class="timeline-heading">
+									<h4 class="timeline-title">Siapa saja yang bisa mendaftar?</h4>
+								</div>
+								<div class="timeline-body">
+									<p> Setiap mahasiswa Universitas Mercu Buana dapat mendaftar. Dari mahasiswa yang mendaftar, dapat mendaftarkan 4 temannya sehingga jumlahnya jadi 5.</p>
+								</div>
+							</div>
+						</li>
 							<li class="timeline-inverted wow bounceInLeft">
 								<div class="timeline-badge success"><i class="glyphicon glyphicon-check"></i></div>
 								<div class="timeline-panel" style="background-color: #FFFFFF;">
@@ -61,36 +74,6 @@
 										<!-- <?php echo $checkmatkulkp." ".$checktranskripkp ; echo "asd" ?> -->
 										<!-- <?php  ?> -->
 									</div>
-								</div>
-							</div>
-						</li>
-						<li class="wow bounceInRight">
-							<div class="timeline-badge danger"><i class="glyphicon glyphicon-user"></i></div>
-							<div class="timeline-panel" style="background-color: #FFFFFF;">
-								<div class="timeline-heading">
-									<h4 class="timeline-title">Siapa saja yang bisa mendaftar?</h4>
-								</div>
-								<div class="timeline-body">
-									<p> Setiap mahasiswa Universitas Mercu Buana dapat mendaftar. Dari mahasiswa yang mendaftar, dapat mendaftarkan 4 temannya sehingga jumlahnya jadi 5.</p>
-								</div>
-							</div>
-						</li>
-						<li class="timeline-inverted wow bounceInLeft">
-							<div class="timeline-badge success"><i class="glyphicon glyphicon-check"></i></div>
-							<div class="timeline-panel" style="background-color: #FFFFFF;">
-								<div class="timeline-heading">
-									<h4 class="timeline-title">Saya sudah memenuhi semua persyaratannya, lalu?</h4>
-								</div>
-								<div class="timeline-body">
-									<?php if ($checkmatkulkp == 0 AND $checktranskripkp ==0 ): ?> 
-										Maaf anda belum bisa mengajukan surat kerja praktek
-									<?php elseif($checkmatkulkp == 1 AND $checktranskripkp == 0 ): ?> 
-										Kunjungi link ini untuk mendaftar > <a href="<?php echo site_url('mahasiswa/formkp') ?>" class="btn btn-danger dash-btn">Form Surat Kp</a>
-									<?php elseif($checkmatkulkp == 1 AND $checktranskripkp == 1 ): ?> 
-										Maaf anda sudah mengambil mata kuliah kerja praktek         <!-- YANG INI NIII-->
-									<?php endif ?> 
-									<!-- <?php echo $checkmatkulkp." ".$checktranskripkp ; echo "asd" ?> -->
-									<!-- <?php  ?> -->
 								</div>
 							</div>
 						</li>
