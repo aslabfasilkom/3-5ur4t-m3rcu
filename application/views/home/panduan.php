e3534bc0f9bfd8aa5c0dd7170e3d5cfe3d09c30a
ivalrivall <ivalrival95@gmail.com>
2018-04-30 11:59:43 +0700

    redesign tampilan panduan
    panduan 7 dan 8 belom kelar

--- application/views/home/panduan.php
+++ application/views/home/panduan.php
@@ -1,140 +1,125 @@
 <section class="tentang">
-<section class="tentangup">
-<div class="container-fluid">
-    <div class="row">
-  <div class="jumbotron tentanghome">
-        <h1 class="texttentang" style="color: #f4f5f7;">PANDUAN</h1>      
-        <p class="texttentang1">Tata Cara Dalam Menggunakan E-Surat Sesuai Dengan Ketentuan</p>
-
-    
-  </div>
-  </div>   
-</div>
-</section>
-<section class="tentang1">
-    <div class="container">
-    <div class="alert alert-info"> 
-  <h2>TATA CARA MENGGUNAKAN E-SURAT</h2>
+  <section class="tentangup">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="jumbotron tentanghome">
+          <h1 class="texttentang" style="color: #f4f5f7;">PANDUAN</h1>
+          <p class="texttentang1">Tata Cara Dalam Menggunakan E-Surat Sesuai Dengan Ketentuan</p>
+        </div>
+      </div>
     </div>
-
-
-  <div class="row" style="margin-bottom: 100px;">
-                <div class="col-md-6 wow bounceInLeft">
-                    <img src="<?php echo base_url('assets/image/daftar.jpg') ?>" alt="esurat home" style="max-width: 100%;" >
-                </div><!--end of panduan image-->
-                <div class="col-md-6 col-lg-6 wow bounceInRight">
-                    <h2>Panduan 1</h2>
-                        <p>Sebagai pengguna baru Anda diharapkan untuk mendaftarkan diri 
-                            Anda dengan cara menekan tombol daftar dan mengisikan formulir yang tersedia 
-                         </p>
-                </div><!--end of panduan description-->
-            </div><!--panduan 1-->
-           
-          
-            <div class="row" style="margin-bottom: 100px;">
-                <div class="col-md-6 col-lg-6 paddingdesckiri wow bounceInRight">
-                    <h2>Panduan 2</h2>
-                        <p>Anda diharuskan untuk mengisi formulir yang <b>sesuai dengan data diri asli yang terdapat di KTM Anda</b>, 
-                            hal ini diperlukan karena data yang Anda isikan akan digunakan untuk keperluan pembuatan surat 
-                            kerja praktek / riset tugas akhir. Jadi diharapkan untuk mengisi formulir dengan benar dan sesuai. 
-                         </p>
-                </div><!--end of panduan description-->
-                <div class="col-md-6 wow bounceInLeft">
-                    <img src="<?php echo base_url('assets/image/daftarkan.JPG') ?>" alt="esurat form register" style="max-width: 100%;" >
-                </div><!--end of panduan image-->
-            </div><!--panduan 2 kiri-->
-            
-            <div class="row" style="margin-bottom: 100px;">
-                <div class="col-md-6 wow bounceInLeft">
-                    <img src="<?php echo base_url('assets/image/login.JPG') ?>" alt="Login" style="max-width: 100%;" >
-                </div><!--end of panduan image-->
-                <div class="col-lg-6 col-md-6 padding-20 wow bounceInRight">
-                    <h2>Panduan 3</h2>
-                        <p>Setelah akun berhasil dibuat Anda dapat masuk dengan menggunakan NIM dan password yang 
-                            sebelumnya telah Anda buat. 
-                         </p>
-                </div><!--end of panduan description-->
-            </div><!--panduan 3-->
-            
-           <div class="row" style="margin-bottom: 100px;">
-                <div class="col-lg-6 col-md-6 paddingdesckiri wow bounceInRight">
-                    <h2>Panduan 4</h2>
-                        <p>Silahkan Anda pilih surat yang ingin dibuat. 
-                         </p>
-                </div><!--end of panduan description-->
-                <div class="col-md-6 wow bounceInLeft">
-                    <img src="<?php echo base_url('assets/image/dashboardpilihan.png') ?>" alt="daftar esurat" style="max-width: 100%;" >
-                </div><!--end of panduan image-->
-            </div><!--panduan 4 kiri-->
-
-
-            <div class="row" style="margin-bottom: 100px;">
-                <div class="col-md-6 wow bounceInLeft">
-                    <img src="<?php echo base_url('assets/image/formkerjapraktek.JPG') ?>" alt="form kerja praktek" style="max-width: 100%;" >
-                </div><!--end of panduan image-->
-                <div class="col-lg-6 col-md-6 padding-20 wow bounceInRight">
-                    <h2>Panduan 5</h2>
-                        <p>Anda diharuskan mengisi formulir yang sesuai dengan perusahaan 
-                            yang dituju. Dalam pembuatan surat kerja praktek Anda dapat membuat 
-                            1 surat yang berisikan maksimal 5 orang anggota 
-                            (termasuk Anda) yang merupakan pengajuan kerja praktek untuk 1 perusahaan yang sama. 
-                         </p>
-                </div><!--end of panduan description-->
-            </div><!--panduan 7-->
-
-            <div class="row" style="margin-bottom: 100px;">
-                <div class="col-lg-6 col-md-6 paddingdesckiri wow bounceInRight">
-                    <h2>Panduan 6</h2>
-                        <p>Isikan NIM dan nama yang sesuai dengan anggota Anda, jika Anda hanya mengajukan pembuatan
-                            surat kerja praktek untuk seorang diri maka Anda bisa melewati langkah ini. 
-                            <br/><br/>
-                            Catatan : Mahasiswa yang akan menerima email pemberitahuan hanya akun mahasiswa A
-                         </p>
-                </div><!--end of panduan description-->
-                <div class="col-md-6 wow bounceInLeft">
-                    <img src="<?php echo base_url('assets/image/anggotakp.jpg') ?>" alt="anggota kp" style="max-width: 100%;" >
-                </div><!--end of panduan image-->
-            </div><!--panduan 8 kiri-->
-
-            <div class="row" style="margin-bottom: 100px;">
-                <div class="col-md-6 wow bounceInLeft">
-                    <img src="<?php echo base_url('assets/image/daftarulang.JPG') ?>" alt="status surat" style="max-width: 100%; padding: 100px 0px; max-height: 30%" >
-                </div><!--end of panduan image-->
-                <div class="col-lg-6 col-md-6 wow bounceInRight">
-                    <h2>Panduan 7</h2>
-                        <p>
-                            Setelah Anda mengisi formulir dan menekan tombol kirim, Anda akan dialihkan 
-                                ke halaman status yang merupakan halaman untuk memeriksa status surat yang Anda ajukan, 
-                                dimana pengertian dari tahapan status pembuatan surat adalah sebagai berikut :
-                                    <ul>
-                                        <li><span style="color:blue;"><b>Menunggu</b></span> : pembuatan surat yang Anda ajukan masih menunggu antrian surat-surat yang mahasiswa lain ajukan.</li>
-                                        <li><span style="color:#ff9900;"><b>Proses</b></span> : surat yang Anda ajukan sudah masuk ke tahap pencetakan dan pentandatanganan</li>
-                                        <li><span style="color:#0BE550;"><b>Selesai</b></span> : surat yang Anda ajukan telah selesai dan Anda akan menerima email berupa pemberitahuan yang berisikan 
-                                                lampiran pdf yang harus Anda cetak sebagai tanda bukti untuk pengambilan surat di Tata Usaha FASILKOM</li>
-                                        <li><span style="color:#4F80E1;"><b>Ambil</b></span> : surat yang Anda ajukan telah selesai dan diterima, pada tahap ini <b>Anda tidak bisa mengajukan pembuatan surat selama 
-                                                2 hari setelah surat Anda terima.</b></li>    
-                                        <li><span style="color:red;"><b>Tolak</b></span> : status ini menandakan bahwa pembuatan surat yang Anda ajukan memiliki kesalahan pada saat mengisi formulir atau 
-                                                informasi yang Anda isikan tidak sesuai. Jika status Anda ditolak Anda dapat langsung mengajukan pembuatan surat kembali</li>    
-                                     </ul>    
-                         </p>
-                </div><!--end of panduan description-->
-            </div><!--panduan 9-->
-            <br/><br/>
-            <div class="row" style="margin-bottom: 100px;">
-                    <div class="col-lg-6 col-md-6 paddingdesckiri wow bounceInRight">
-                        <h2>Panduan 8</h2>
-                            <p>Setelah Anda mendapatkan email pemberitahuan bahwa surat Anda telah selesai Anda akan mendapatkan email 
-                                yang berisikan lampiran untuk mencetak lampiran tersebut sebagai tanda bukti pengambilan surat yang 
-                                telah Anda ajukan.
-                                <br /><br />
-                                Setelah surat Anda terima dari Tata Usaha FASILKOM Anda dan anggota Anda (jika ada) tidak bisa melakukan
-                                pengajuan pembuatan surat selama 2 hari dimulai dari waktu Anda mengambil surat di Tata Usaha FASILKOM. 
-                             </p>
-                    </div><!--end of panduan description-->
-                    <div class="col-md-6 paddingk-0 wow bounceInLeft">
-                        <img src="<?php echo base_url('assets/image/jeda.JPG') ?>" alt="Print" style="max-width: 100%;" >
-                    </div><!--end of panduan image-->
-                </div><!--panduan 10 kiri-->
+  </section>
+  <section class="tentang1">
+    <div class="container">
+    <div class="alert alert-info m-b-35 align-center">
+      <h2>TATA CARA MENGGUNAKAN E-SURAT</h2>
+    </div>
+    <!-- panduan 1 -->
+    <div class="row m-b-60">
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 wow bounceInLeft m-b-20">
+        <img src="<?php echo base_url('assets/image/panduan_dashboard.jpg')?>" alt="dashboard" style="max-width: 100%;">
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 wow bounceInRight">
+        <span class="font-30">Panduan 1</span><br>
+        <p class="align-justify">Sebagai pengguna baru anda diharapkan untuk daftar dengan cara menekan tombol daftar dan mengisikan formulir yang tersedia</p>
+      </div>
+    </div>
+    <!-- end panduan 1-->
+    <!-- panduan 2 -->
+    <div class="row m-b-60">
+      <div class="col-sm-12 col-xs-12 col-md-6 col-lg-6 wow bounceInRight m-b-20">
+        <img src="<?php echo base_url('assets/image/panduan_daftar.jpg')?>" alt="tampilan daftar" style="max-width: 100%;">
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 wow bounceInLeft">
+        <span class="font-30">Panduan 2</span><br>
+        <p class="align-justify">Anda diharuskan untuk mengisi formulir yang <b>sesuai dengan data diri asli yang terdapat di Kartu Tanda Mahasiswa (KTM) anda</b>, hal ini diperlukan karena data yang anda isikan akan digunakan untuk keperluan pembuatan surat kerja praktek / riset tugas akhir.&nbsp;&nbsp;Jadi diharapkan untuk mengisi formulir dengan benar dan sesuai.</p>
+      </div>
+    </div>
+    <!-- end panduan 2-->
+    <!-- panduan 3 -->
+    <div class="row m-b-60">
+      <div class="col-sm-12 col-xs-12 col-md-6 col-lg-6 wow bounceInLeft m-b-20">
+        <img src="<?php echo base_url('assets/image/panduan_login.png')?>" alt="tampilan login" style="max-width: 100%;">
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 wow bounceInRight">
+        <span class="font-30">Panduan 3</span><br>
+        <p class="align-justify">Setelah anda berhasil mendaftar, anda dapat masuk/login dengan menggunakan NIM/email dan password yang 
+        sebelumnya telah anda daftarkan.</p>
+      </div>
+    </div>
+    <!--end panduan 3-->
+    <!-- panduan 4 -->
+    <div class="row m-b-60">
+      <div class="col-sm-12 col-xs-12 col-md-6 col-lg-6 wow bounceInRight m-b-20">
+        <img src="<?php echo base_url('assets/image/panduan_loggedin_dashboard.png')?>" alt="pilih surat" style="max-width: 100%;" >
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 wow bounceInLeft">
+        <span class="font-30">Panduan 4</span>
+        <p class="align-justify">Silahkan anda pilih surat yang ingin dibuat</p>
+      </div>
+    </div>
+    <!--end panduan 4-->
+    <!-- panduan 5 -->
+    <div class="row m-b-60">
+      <div class="col-sm-12 col-xs-12 col-md-6 col-lg-6 wow bounceInLeft m-b-20">
+        <img src="<?php echo base_url('assets/image/panduan_formkp.png')?>" alt="form kerja praktek" style="max-width: 100%;" >
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 wow bounceInRight">
+        <span class="font-30">Panduan 5</span>
+        <p class="align-justify">Anda diharuskan mengisi formulir yang sesuai dengan perusahaan yang dituju. Dalam pembuatan surat kerja praktek Anda dapat membuat 1 surat yang berisikan maksimal 5 orang anggota (termasuk Anda) yang merupakan pengajuan kerja praktek untuk 1 perusahaan yang sama. 
+        </p>
+      </div>
+    </div>
+    <!--end panduan 5-->
+    <!--panduan 6-->
+    <div class="row m-b-60">
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 wow bounceInRight m-b-20">
+        <img src="<?php echo base_url('assets/image/panduan_tambahanggota.png') ?>" alt="anggota kp" style="max-width: 100%;" >
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 wow bounceInLeft">
+        <span class="font-30">Panduan 6</span>
+        <p class="align-justify">Isikan NIM dan nama yang sesuai dengan anggota Anda, jika Anda hanya mengajukan pembuatan surat kerja praktek untuk seorang diri maka Anda bisa melewati langkah ini.<br><br>Catatan : Akun yang akan menerima email pemberitahuan hanya akun mahasiswa yang mengajukan surat
+        </p>
+      </div>
+    </div>
+    <!--end panduan 6-->
+    <!--panduan 7 -->
+    <div class="row m-b-60">
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 wow bounceInLeft m-b-20">
+        <img src="<?php echo base_url('assets/image/daftarulang.JPG') ?>" alt="status surat" style="max-width: 100%;">
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 wow bounceInRight">
+        <span class="font-30">Panduan 7</span>
+        <p class="align-justify">Setelah Anda mengisi formulir dan menekan tombol kirim, Anda akan dialihkan ke halaman status yang merupakan halaman untuk memeriksa status surat yang Anda ajukan, dimana pengertian dari tahapan status pembuatan surat adalah sebagai berikut :
+        <ul class="align-justify">
+          <li><span style="color:blue;"><b>Menunggu</b></span> : pembuatan surat yang Anda ajukan masih menunggu antrian surat-surat yang mahasiswa lain ajukan.</li>
+          <li><span style="color:#ff9900;"><b>Proses</b></span> : surat yang Anda ajukan sudah masuk ke tahap pencetakan dan pentandatanganan</li>
+          <li><span style="color:#0BE550;"><b>Selesai</b></span> : surat yang Anda ajukan telah selesai dan Anda akan menerima email berupa pemberitahuan yang berisikan 
+            lampiran pdf yang harus Anda cetak sebagai tanda bukti untuk pengambilan surat di Tata Usaha FASILKOM
+          </li>
+          <li><span style="color:#4F80E1;"><b>Ambil</b></span> : surat yang Anda ajukan telah selesai dan diterima, pada tahap ini <b>Anda tidak bisa mengajukan pembuatan surat selama 
+            2 hari setelah surat Anda terima.</b>
+          </li>
+          <li><span style="color:red;"><b>Tolak</b></span> : status ini menandakan bahwa pembuatan surat yang Anda ajukan memiliki kesalahan pada saat mengisi formulir atau 
+            informasi yang Anda isikan tidak sesuai. Jika status Anda ditolak Anda dapat langsung mengajukan pembuatan surat kembali
+          </li>
+        </ul>
+        </p>
+      </div>
+    </div>
+    <!-- end panduan 7 -->
+    <!--panduan 8-->
+    <br/><br/>
+    <div class="row m-b-60">
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 wow bounceInRight m-b-20">
+        <img src="<?php echo base_url('assets/image/jeda.JPG') ?>" alt="Print" style="max-width: 100%;" >
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 wow bounceInLeft">
+        <span class="font-30">Panduan 8</span>
+        <p class="align-justify">Setelah Anda mendapatkan email pemberitahuan bahwa surat Anda telah selesai Anda akan mendapatkan email yang berisikan lampiran untuk mencetak lampiran tersebut sebagai tanda bukti pengambilan surat yang telah Anda ajukan.<br><br>Setelah surat Anda terima dari Tata Usaha FASILKOM Anda dan anggota Anda (jika ada) tidak bisa melakukan pengajuan pembuatan surat selama 2 hari dimulai dari waktu Anda mengambil surat di Tata Usaha FASILKOM. 
+        </p>
+      </div>
+    </div>
+    <!--end panduan 8-->
+  </section>
 </section>
-</section>
-
